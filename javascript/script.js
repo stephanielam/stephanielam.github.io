de807f9dba278a493dabe4faa1aedea0a2d1b571
Stephanie Lam <m.stephanielam@gmail.com>
2017-08-27 22:57:21 -0700

    Add files

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+$(function(){
+  console.log("jquery");
+  $('#renadrone').click(function(){
+    window.location = 'https://github.com/stephanielam/renadrone';
+  });
+
+  $('#bhmm').click(function(){
+    window.location = 'https://github.com/leimd/Midterm_Project';
+  });
+
+  $('#kairos').click(function(){
+    window.location = 'https://github.com/stephanielam/kairos';
+  });
+
+  $('#cashout').click(function(){
+    window.location = 'http://toey.herokuapp.com';
+  });
+
+  $('#fbadhair').click(function(){
+    window.location = 'http://fuckbadhair.com';
+  });
+
+  $('#cvc').click(function(){
+    window.location = 'http://ubccvc.ca/';
+  });
+
+  $('#lam').click(function(){
+    window.location = 'http://stephanielam.github.io';
+  });
+});
